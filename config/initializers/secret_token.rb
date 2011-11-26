272c9da11051c0a30a13f37d61f791cd125bf311
AugustoGu <joaoaugusto1@gmail.com>
2011-11-26 19:26:10 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmaMonitoramento::Application.config.secret_token = '3ea9d4ebddc32d9c3b04b29c525124d917fe10f40d1b495d60ee03ade5b872e0039cb9a6ac7adc60bd90b88024cd25b7305c537eb2cff8cd34849673e673c078'
