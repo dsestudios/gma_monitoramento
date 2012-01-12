186e1c5e0c587ceb06bd1ee57a4e0a01a5aeab39
AugustoGu <joaoaugusto1@gmail.com>
2012-01-11 23:15:16 -0300

    ajustes

--- app/controllers/visores_controller.rb
+++ app/controllers/visores_controller.rb
@@ -5,81 +5,41 @@ class VisoresController < ApplicationController
   # GET /visores.xml
   def index
     @visores = Visor.all.order_by([[:nome, :asc]])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @visores }
-    end
+    respond_to_index(@visores)
   end
 
   # GET /visores/1
   # GET /visores/1.xml
   def show
-    @visor = Visor.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @visor }
-    end
+    @visor = respond_to_show(Visor)
   end
 
   # GET /visores/new
   # GET /visores/new.xml
   def new
-    @visor = Visor.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @visor }
-    end
+    @visor = respond_to_new(Visor)
   end
 
   # GET /visores/1/edit
   def edit
-    @visor = Visor.find(params[:id])
+    @visor = respond_to_edit(Visor)
   end
 
   # POST /visores
   # POST /visores.xml
   def create
-    @visor = Visor.new(params[:visor])
-
-    respond_to do |format|
-      if @visor.save
-        format.html { redirect_to(@visor, :notice => t("messages.notice.new_registro", :model => Visor.nome_exibicao ) ) }
-        format.xml  { render :xml => @visor, :status => :created, :location => @visor }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @visor.errors, :status => :unprocessable_entity }
-      end
-    end
+    @visor = respond_to_create(Visor)
   end
 
   # PUT /visores/1
   # PUT /visores/1.xml
   def update
-    @visor = Visor.find(params[:id])
-
-    respond_to do |format|
-      if @visor.update_attributes(params[:visor])
-        format.html { redirect_to(visores_path, :notice => t("messages.notice.edit_registro", :model => Visor.nome_exibicao )) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @visor.errors, :status => :unprocessable_entity }
-      end
-    end
+    @visor = respond_to_update(Visor)
   end
 
   # DELETE /visores/1
   # DELETE /visores/1.xml
   def destroy
-    @visor = Visor.find(params[:id])
-    @visor.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(visores_url) }
-      format.xml  { head :ok }
-    end
+    @visor = respond_to_destroy(Visor)
   end
 end
