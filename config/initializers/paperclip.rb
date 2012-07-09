e3947a756bf6352184b263700f8017cad416e535
Joao Augusto <joaoaugusto1@gmail.com>
2012-07-09 18:57:22 -0300

    alteracao do timeout agora = 60 minutos

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,4 @@
+#require "paperclip"
+
+#É necessario fazer esta configuração especial quando esta usando ruby 1.9.3 e webrick, é uma falha do webrick
+#Paperclip.options[:command_path] = "C:\Program Files\ImageMagick-6.7.8-Q16"
